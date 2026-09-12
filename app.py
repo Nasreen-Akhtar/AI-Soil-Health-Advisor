e12b1dfb8ab9ebf03bc280fa1799de6591cc8d1b
Nasreen-Akhtar <nasreenakh806@gmail.com>
2026-09-12 11:35:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,12 @@ if st.button("Analyze Soil"):
 
     st.subheader("🔬 Soil Analysis")
     st.write(f"**pH Analysis:** {ph_result}")
+    if organic_matter < 2.0:
+        organic_result = "Your soil organic matter is low."
+    elif organic_matter <= 4.0:
+        organic_result = "Your soil organic matter is in a reasonable range."
+    else:
+        organic_result = "Your soil has high organic matter."
 
     st.subheader("🌱 Soil Information")
 
